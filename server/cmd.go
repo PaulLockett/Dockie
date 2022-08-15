ca6a182f0fa79d5e203683d4853802e34005fe8f
Paul Lockett <Plockett.pl@gmail.com>
2022-08-15 04:53:12 -0700

    split out storage and handlers
    Signed-off-by: Paul Lockett <Plockett.pl@gmail.com>

---
+++ server/cmd.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"app/handlers"
+	"app/models"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	startupTime := time.Now()
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("setup: %v", err)
+	}
+
+	env := &handlers.Env{
+		StartTime: startupTime,
+		Storage: &models.StorageModel{
+			Client: client,
+		},
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/_ah/warmup", env.WarmUpHandler)
+
+	r.HandleFunc("/", env.IndexHandler).Methods("GET")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	log.Printf("Listening on port %s", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
